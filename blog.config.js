c7c57f7c778c806dcf0f1af35ce4708a5b4fd035
Komaeda04285 <114289474+Komaeda04285@users.noreply.github.com>
2023-05-10 18:36:18 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '星星岛的竹小竹', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '迷茫的列车就像旅途上的人们。', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://starsbamboo.icu', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '浪漫鉄道', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
